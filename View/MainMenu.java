7f46f739ab12766e9c461aa8846745768b6c1e02
StanislausJoel <stanislausjoel.fa@gmail.com>
2024-06-20 14:30:38 +0700

    add view

---
+++ View/MainMenu.java
@@ -0,0 +1,17 @@
+package View;
+
+import Controller.Controller;
+
+public class MainMenu {
+    
+    private Controller controller;
+
+    public MainMenu(Controller controller) {
+
+        this.controller = controller;
+
+    }
+
+    
+
+}
